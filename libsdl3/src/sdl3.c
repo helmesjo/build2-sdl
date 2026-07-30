ef256f1b182f99ab33ff4a19055c324a2551e1ba
helmesjo <helmesjo@gmail.com>
2026-07-30 18:43:30 +0200

    bdep new --package --lang c --type lib,split,subdir=SDL3,no-subdir-source,no-version,no-symexport,third-party,buildfile-in-prefix libsdl3

---
+++ libsdl3/src/sdl3.c
@@ -0,0 +1,14 @@
+#include <SDL3/sdl3.h>
+
+#include <errno.h>
+
+int say_hello (FILE *f, const char* n)
+{
+  if (f == NULL || n == NULL || *n == '\0')
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  return fprintf (f, "Hello, %s!\n", n);
+}
