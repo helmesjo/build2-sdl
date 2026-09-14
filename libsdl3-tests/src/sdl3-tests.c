75a37becad17245325ba9ceedb0dfd7d985e5b7c
helmesjo <helmesjo@gmail.com>
2026-09-14 14:05:13 +0200

    bdep new --package --lang c --type lib,subdir=src,no-version,no-symexport,no-tests,third-party libsdl3-tests

---
+++ libsdl3-tests/src/sdl3-tests.c
@@ -0,0 +1,14 @@
+#include <src/sdl3-tests.h>
+
+#include <errno.h>
+
+int say_hello (FILE *f, const char* n)
+{
+  if (f == NULL || n == NULL || *n == '\0')
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  return fprintf (f, "Hello, %s!\n", n);
+}
